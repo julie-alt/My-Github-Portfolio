6ebfd1feb26c7637e09f3de650efef47698c2c90
julie-alt <julietnwosu2000@gmail.com>
2024-04-22 11:59:50 +0100

    made modifications for pagination and search

--- src/pages/RepoList.jsx
+++ src/pages/RepoList.jsx
@@ -5,41 +5,35 @@ import ResponsiveGrid from "../assets/Grid";
 function RepoList() {
   const [repos, setRepos] = useState([]);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetchRepos();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+  }, [page, searchQuery]);
 
   const fetchRepos = async () => {
     try {
-      const response = await axios.get(
-        `https://api.github.com/users/julie-alt/repos`,
-        {
-          params: {
-            page,
-            per_page: 4,
-          },
-          headers: {
-            Authorization: "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",
-          },
-        }
-      );
-
-      const linkHeader = response.headers.link;
-      let totalPages = 1;
-
-      if (linkHeader) {
-        const pageRegex = /&page=(\d+)& rel="last"/;
-        const match = linkHeader.match(pageRegex);
-        if (match) {
-          totalPages = parseInt(match[1]);
-          console.log("Total pages:", totalPages);
-        }
+      let url = `https://api.github.com/user/139226169/repos`;
+      const params = {
+        page,
+        per_page: 2,
+      };
+      if (searchQuery) {
+        url = `https://api.github.com/search/repositories`;
+        params.q = `${searchQuery} in:name user:julie-alt`;
       }
-      setRepos(response.data);
-      setTotalPages("Total pages:", totalPages);
+      const response = await axios.get(url, {
+        params,
+        headers: {
+          Authorization: "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",
+        },
+      });
+      if (searchQuery) {
+        setRepos(response.data.items);
+      } else {
+        setRepos(response.data);
+      }
     } catch (error) {
       console.error("Error fetching repositories:", error);
     }
@@ -50,22 +44,29 @@ function RepoList() {
   };
 
   const handleNextPage = () => {
-    setPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    setPage((prevPage) => Math.min(prevPage + 1));
   };
+
+ 
   return (
     <div>
       <h1 className="profile">Github Repositories</h1>
+      <input
+      className="search-input"
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search repositories..."
+      />
+      <button className="search-btn" onClick={fetchRepos}>Search</button>
+      <br /> <br />
       <ResponsiveGrid repos={repos} /> <br />
       <div>
         <button onClick={handlePrevPage} disabled={page === 1}>
           Previous
         </button>
-        <span>
-          Page {page} of {totalPages}
-        </span>
-        <button onClick={handleNextPage} disabled={page === totalPages}>
-          Next
-        </button>
+
+        <button onClick={handleNextPage}>Next</button>
       </div>
     </div>
   );
